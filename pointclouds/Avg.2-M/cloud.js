fe9a7ef1d6d575c7800fd938126eb962cd04cef0
jckenny59 <joseph.kenny@jacks.sdstate.edu>
2021-12-11 15:24:10 -0600

    Initial-Push

---
+++ pointclouds/Avg.2-M/cloud.js
@@ -0,0 +1,59 @@
+{
+    "version": "1.8",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 67524093,
+    "boundingBox": {
+        "lx": -11.079000473022461,
+        "ly": -10.807000160217286,
+        "lz": -0.000102996826171875,
+        "ux": 103.55299949645996,
+        "uy": 103.82499980926514,
+        "uz": 114.63189697265625
+    },
+    "tightBoundingBox": {
+        "lx": -11.078999999999999,
+        "ly": -10.807,
+        "lz": -0.000103,
+        "ux": 12.696999,
+        "uy": 12.373,
+        "uz": 114.631897
+    },
+    "pointAttributes": [
+        {
+            "name": "POSITION_CARTESIAN",
+            "size": 12,
+            "elements": 3,
+            "elementSize": 4,
+            "type": "int32",
+            "description": ""
+        },
+        {
+            "name": "RGBA",
+            "size": 4,
+            "elements": 4,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "number of returns",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "return number",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        }
+    ],
+    "spacing": 0.9927422404289246,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
